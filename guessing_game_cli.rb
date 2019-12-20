c758fe5a39c6e9da5657d2778806754c9c923c67
Dan Foley <dan@dan-foley.com>
2019-12-20 05:42:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 require "pry"
 # Code your solution here!
 
-def run_guessing_game
+# def run_guessing_game
 #   secret_num = rand(1..6).to_s
 #   puts "Pick a number between 1 and 6"
 #   user_input = gets.chomp
